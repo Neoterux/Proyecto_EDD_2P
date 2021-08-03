1ba53e9a9a2c0d232707d0c4032b5788bd8aa47e
Marco Del Rosario <marsdel@espol.edu.ec>
2021-08-02 22:38:42 -0500

    implementacion de ventana de inicio de juego

--- src/main/java/com/neoterux/sttkoe/view/controllers/MainViewController.java
+++ src/main/java/com/neoterux/sttkoe/view/controllers/MainViewController.java
@@ -1,9 +1,35 @@
 package com.neoterux.sttkoe.view.controllers;
 
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+
 /**
  * Controller for the main View
  *
  * @author Neoterux
  */
 public class MainViewController {
+
+    @FXML
+    private ChoiceBox<String> choicePlay = new ChoiceBox<>(FXCollections.observableArrayList(
+            "Player vs Player",
+            "Player vs CPU",
+            "CPU vs CPU"));
+
+    @FXML
+    private ChoiceBox<String> symbolPlayer = new ChoiceBox<>(FXCollections.observableArrayList("X", "O"));
+
+    @FXML
+    private ChoiceBox<String> symbolEnemy = new ChoiceBox<>(FXCollections.observableArrayList("X", "O"));
+
+    @FXML
+    private Button btnInicio;
+
+    @FXML
+    void ComenzarJuego(ActionEvent event) {
+
+    }
 }
